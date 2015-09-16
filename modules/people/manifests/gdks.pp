036660bf972f380973f31d6884fbf719ea3593f0
Gavin Stewart <gavin@gdks.co.uk>
2015-09-16 20:42:23 +0100

    Create people manifest for me

---
+++ modules/people/manifests/gdks.pp
@@ -0,0 +1,14 @@
+class people::gdks {
+
+  include iterm2::dev
+  include wget
+  include zsh
+
+  include chrome
+  include dropbox
+  include flowdock
+  include skype
+
+  include virtualbox
+  include vagrant
+}
